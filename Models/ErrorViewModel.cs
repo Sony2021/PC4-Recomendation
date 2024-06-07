d1ac89be9f2c42dd33c1cb5e049af281a4f04ca0
Sony2021 <sony_barrios@usmp.pe>
2024-06-07 16:50:51 -0500

    plantilla

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PC4_Recomendation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
